64969bdd80d8f7dd9f0bac93191c387d121ed2ca
SahanHeshan <dshsamarathunga@gmail.com>
2025-07-11 13:27:15 +0530

    fix nev icon

--- seemo-front/app/page.tsx
+++ seemo-front/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "@/components/hero-section";
 import IntegrationsSection from "@/components/integrations-3";
 import LogoCloud from "@/components/logo-cloud";
